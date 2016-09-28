902ac2ece6c3ff3bbcd86ea51280a7847a59dd3d
rhinodavid <dawalsh@gmail.com>
2016-09-28 16:32:25 -0700

    Implement song queue to pass tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,28 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', this.handleAddSong, this);
+    this.on('dequeue', this.handleDequeue, this);
+  },
+
+  playFirst: function() {
+    // call play on the first (only) item in the collection
+    this.at(0).play();
+  },
+
+  handleAddSong: function(model, collection, options) {
+    // if it's the only song in the queue,
+    // we'll play it.
+    if (collection.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  handleDequeue: function(song) {
+    this.remove(song);
+    if (this.length) {
+      this.playFirst();
+    }
   }
 
 });
