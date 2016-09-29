6839ae9d4174fcacf0b28627dbbc5eabd383ccb0
rhinodavid <dawalsh@gmail.com>
2016-09-28 17:03:08 -0700

    Complete song queue view to pass tests

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,18 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+
+  template: _.template('<li class="song"> \
+   <span class="artist"><%- artist %></span> \
+   <span class="title"><%- title %></span></li>'),
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,23 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'div',
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    // render each song item and put them together into the $el
+    this.$el.empty();
+    this.$el.append('<h1>Song Queue</h1>');
+    this.collection.forEach(this.renderSongEntry, this);
     return this.$el;
+  },
+
+  renderSongEntry: function(song) {
+    var songEntryView = new SongQueueEntryView({model: song});
+    this.$el.append(songEntryView.render());
   }
 
 });
